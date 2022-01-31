47b9d129d3c440acb979f7863810e3913a06fd7f
Kowshikkrishna <kowshikkrishna4@gmail.com>
2022-01-31 17:04:46 +0530

    comments

---
+++ EvenOrOdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        int n;
+        Console.WriteLine("Enter the Number :");
+        n = int.Parse(Console.ReadLine());  
+        if(n>=18)
+        {
+            Console.WriteLine("{0} number is Even");
+        }
+        else{
+            Console.WriteLine("{0} number is Odd",n);
+        }
+        }
+    }
+}
